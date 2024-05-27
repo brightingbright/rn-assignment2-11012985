a7018e7cb7dc413090cff13e1039c8675842ea8e
BrightAgbemenu <136804517+BrightAgbemenu@users.noreply.github.com>
2024-05-27 14:20:31 +0000

    Update App.js
    Added the main  content of the app

--- App.js
+++ App.js
@@ -1,3 +1,13 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        My name is <Text style={styles.boldText}>Bright Agbemenu</Text>
+      </Text>
+    </View>
+  );
+}
+
