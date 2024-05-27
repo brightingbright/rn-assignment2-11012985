80d2d9d841015fbe3a35cd4d8ec87d76aacad2e7
BrightAgbemenu <136804517+BrightAgbemenu@users.noreply.github.com>
2024-05-27 14:30:17 +0000

    Update App.js
    Updated my background color

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "lightblue",
+    backgroundColor: "green",
     justifyContent: "center",
     alignItems: "center",
   },
